a05e6047d47157ed09cc32aa7a62fcec81529378
HilmiMubarok <m.hilmimubarok@gmail.com>
2022-06-10 13:32:08 +0700

    update environment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  HeaderApiKeyName: 'X-RapidAPI-Key',
+  HeaderApiKeyValue: 'dda4edd1d8msh700c0e4d54db17ap1fab8cjsn94a8bede20e9',
+  HeaderApiHostName: 'X-RapidAPI-Host',
+  HeaderApiHostValue: 'community-open-weather-map.p.rapidapi.com',
 };
